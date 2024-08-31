0d88033b576c2e19fbde836600b357cb8f54f594
Rohan9277373 <zodxankit@gmail.com>
2024-08-31 16:25:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("6829053215:AAGOwcf5NaVSH5fumqLkxZWsVcmK_iDMN_4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@ROHAN_927")
+OWNER_USERNAME=("ROHAN_927")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1352434941")
