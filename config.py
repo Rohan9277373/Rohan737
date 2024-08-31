8740b60d3b83dcc66f669e59c9d3660193aa3f03
Rohan9277373 <zodxankit@gmail.com>
2024-08-31 15:53:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("6829053215:AAGOwcf5NaVSH5fumqLkxZWsVcmK_iDMN_4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("@ROHAN_927")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("")
+ADMIN_IDS=("1352434941")
 
 
 
